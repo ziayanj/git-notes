26f664e4c1e2c5440de53cd63859d085bd74fe60
Ziayan Javed <ziayanj@gmail.com>
2024-01-25 11:35:21 +0500

    Finalize the backend of the application

---
+++ git-notes-backend/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+require('dotenv').config()
+
+const indexRouter = require('./routes/index');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors({ origin: '*' }));
+
+app.use('/', indexRouter);
+
+module.exports = app;

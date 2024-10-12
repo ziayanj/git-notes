3a4c89bec62f0d8a6a05cbf53245c4a94a2a9200
Ziayan Javed <ziayanj@gmail.com>
2024-10-12 23:34:32 +0500

    Handle error while communicating with GitHub

--- git-notes-backend/routes/index.js
+++ git-notes-backend/routes/index.js
@@ -8,23 +8,29 @@ router.get('/', function(req, res, next) {
 });
 
 router.post("/authenticate", async (req, res) => {
-  const { code } = req.body;
-
-  const response = await axios.post('https://github.com/login/oauth/access_token', {
-    client_id: process.env.GITHUB_CLIENT_ID,
-    client_secret: process.env.GITHUB_CLIENT_SECRET,
-    code
-  });
-
-  const params = new URLSearchParams(response.data);
-  const access_token = params.get("access_token");
-
-  const userInfo = await axios.get('https://api.github.com/user', { headers: { 'Authorization': `Bearer ${access_token}` } });
-
-  res.json({
-    user: userInfo.data,
-    token: access_token,
-  });
+  try {
+    const { code } = req.body;
+  
+    const response = await axios.post('https://github.com/login/oauth/access_token', {
+      client_id: process.env.GITHUB_CLIENT_ID,
+      client_secret: process.env.GITHUB_CLIENT_SECRET,
+      code
+    });
+  
+    const params = new URLSearchParams(response.data);
+    const access_token = params.get("access_token");
+  
+    const userInfo = await axios.get('https://api.github.com/user', { headers: { 'Authorization': `Bearer ${access_token}` } });
+  
+    res.json({
+      user: userInfo.data,
+      token: access_token,
+    });
+  } catch (e) {
+    res.status(400).json({
+      error: 'Error while communicating with GitHub',
+    });
+  }
 });
 
 module.exports = router;
